48ef82a8b421e6b556c21ece17b2b2ac22049870
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-03-06 11:43:48 +0100

    First phase

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+        let cavas = document.getElementById('canvas')
+        let ctx = canvas.getContext('2d')
+        document.getElementById('start-btn').onclick = () => {
+            startGame();
+        }
+    };
+  
+    function startGame() {
+      
+    }
+  };
